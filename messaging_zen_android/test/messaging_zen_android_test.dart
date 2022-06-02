f9433dab4c891d2f0778025c2431af5ca92da799
Matthew Rideout <matthew@rideout.studio>
2022-06-02 19:40:12 -0400

    Removed obsolete test.

--- messaging_zen_android/test/messaging_zen_android_test.dart
+++ messaging_zen_android/test/messaging_zen_android_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:messaging_zen_android/messaging_zen_android.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('messaging_zen_android');
@@ -15,9 +14,5 @@ void main() {
 
   tearDown(() {
     channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await MessagingZenAndroid.platformVersion, '42');
   });
 }
